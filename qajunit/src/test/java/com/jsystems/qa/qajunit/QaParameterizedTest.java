2c831d79a04dd4377b7be7519dd2ca9e6212ba6f
„testautomation112019 <testautomation112019@wp.pl>
2019-11-19 10:52:41 +0100

    Parameterized test

---
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/QaParameterizedTest.java
@@ -0,0 +1,50 @@
+package com.jsystems.qa.qajunit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QaParameterizedTest extends ConfigJunit {
+
+
+//    @Test
+    @ParameterizedTest(name = "Parameter test with value {0}")
+    @ValueSource(ints = {5, 15, 25})
+    public void firsParameterizedTest(int number) {
+
+            assertTrue(number % 5 == 0);
+
+//        int[] ints = {5, 16, 25};
+//        for(int i = 0; i <ints.length ; i++) {
+//        }
+
+    }
+
+    @ParameterizedTest(name = "Parameterized test with value {0}")
+    @ValueSource(strings = {"Hello", "Hello junit", "Hello students"})
+    public void secondParameterizedTest(String text) {
+
+        assertTrue(text.contains("Hello"));
+
+    }
+
+    @ParameterizedTest(name = "Parameterized test with value {0} and {1}")
+    @CsvSource(value = {"Hello, 5", "Hello junit 5, 15", "'Hello, 5!', 25"}, delimiter = ',')
+    public void nextParameterizedTest(String text, int number) {
+        assertTrue(text.contains("Hello"));
+        assertTrue(number % 5 == 0);
+
+    }
+
+    @ParameterizedTest(name = "Parameterized test with value {0} and {1}")
+    @CsvFileSource(resources = "/plik.csv", delimiter = ',')
+    public void csvFileParameterizedTest(String text, int number) {
+        assertTrue(text.contains("Hello"));
+        assertTrue(number % 5 == 0);
+
+    }
+}
