f0987be4ed6392245a43af0b25bce525466d9275
„testautomation112019 <testautomation112019@wp.pl>
2019-11-21 09:50:13 +0100

    actions

--- qagui/src/test/java/com/jsystems/qa/qagui/ActionTest.java
+++ qagui/src/test/java/com/jsystems/qa/qagui/ActionTest.java
@@ -1,11 +1,14 @@
 package com.jsystems.qa.qagui;
 
+import com.jsystems.qa.qagui.page.LoginPage;
 import com.jsystems.qa.qagui.page.MainWordpressPage;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("FrontTest")
 public class ActionTest extends ConfigFrontend {
@@ -28,29 +31,27 @@ public class ActionTest extends ConfigFrontend {
         action.build().perform();
     }
 
-//    @Test
-//    public void actionNextTest() {
-//
-//        driver.get(baseUrl);
-//        mainPage = new MainPage(driver);
-//        mainPage.logIn.click();
-//        loginPage = new LoginPage(driver);
-//
-//        // akcje możemy budować wielo-etapowe
-//        Actions action = new Actions(driver);
-//        action.moveToElement(loginPage.usernameInput)
-//                .sendKeys(TestDataStatic.login)
-//                .moveToElement(loginPage.buttonContinue)
-//                // jak po . klikniecie Ctrl + space to rozwinie wam się lista dostępnych metod z klasy Actions
-//                .click()
-//                .build().perform();
-//
-//        passwordPage = new PasswordPage(driver);
-//        passwordPage.waitForVisibilityOfElement(passwordPage.passwordInput, 15);
-//        passwordPage.isContentPresent();
-//    }
-//
-//
+    @Test
+    public void actionNextTest() {
+
+        driver.navigate().to(Configuration.BASE_URL);
+        MainWordpressPage mainPage = new MainWordpressPage(driver);
+        mainPage.loginIcon.click();
+        LoginPage loginPage = new LoginPage(driver);
+
+        // akcje możemy budować wielo-etapowe
+        Actions action = new Actions(driver);
+        action.moveToElement(loginPage.usernameInput)
+                .sendKeys(Configuration.LOGIN)
+                .moveToElement(loginPage.usernameButton)
+                // jak po . klikniecie Ctrl + space to rozwinie wam się lista dostępnych metod z klasy Actions
+                .click()
+                .build().perform();
+
+        loginPage.waitForElementToBeVisibility(loginPage.inputPassword, 15);
+        assertTrue(loginPage.inputPassword.isDisplayed());
+    }
+
     @Test
     public void testBasicRInteraction() {
         Actions do42 = new Actions(driver);
